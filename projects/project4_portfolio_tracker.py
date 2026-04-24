0268293a07596ab0a839d9167ef37f4fbd2123e6
Luis Alicea <chonny507@gmail.com>
2026-04-23 20:14:54 -0400

    add project4, add portfolio dictionary

---
+++ projects/project4_portfolio_tracker.py
@@ -0,0 +1,12 @@
+# Project 4 - Portfolio Tracker
+# Description: tracks crypto holdings, values, and portfolio allocation
+# Author: Luis
+# Date: 2026-04-17
+
+portfolio = {
+    "Bitcoin":  {"amount": 0.5, "buy_price": 60000.00, "current_price": 77234.32},
+    "Ethereum": {"amount": 3.0, "buy_price": 2000.00, "current_price": 2471.48},
+    "Solana": {"amount": 10.0, "buy_price": 120.00, "current_price": 89.40},
+    "Chainlink": {"amount": 50.0, "buy_price": 8.00, "current_price": 9.68}, 
+    "Hyperlink": {"amount": 20.0, "buy_price": 30.00, "current_price": 44.22}
+}
